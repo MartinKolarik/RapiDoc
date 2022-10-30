8edbce746581e6fa52015dea1c7f13951ac72bc2
Martin Kolárik <martin@kolarik.sk>
2022-10-30 20:56:56 +0100

    Update custom-styles.js

--- src/styles/custom-styles.js
+++ src/styles/custom-styles.js
@@ -5,6 +5,25 @@ This file is reserved for any custom css that developers want to add to
 customize their theme. Simply add your css to this file and yarn build.
 */
 
+// language=CSS
 export default css`
+:host {
+    --font-size-small: 13px;
+}
 
+#nav-bar-search {
+    border-color: var(--nav-bg-color) !important;
+}
+
+#nav-bar-search + div {
+    display: none;
+}
+
+.nav-bar > div:first-of-type {
+    padding: 8px 14px 12px 15px !important;
+}
+
+.nav-bar-path {
+    font-size: calc(var(--font-size-small) + 0px);
+}
 `;
